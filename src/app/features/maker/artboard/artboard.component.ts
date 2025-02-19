b2bfedf2bf1e9763ec28d9b339ccb350a57f28ba
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-02-19 19:41:46 +0100

    arquitectura maker

---
+++ src/app/features/maker/artboard/artboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artboard',
+  imports: [],
+  templateUrl: './artboard.component.html',
+  styleUrl: './artboard.component.css'
+})
+export class ArtboardComponent {
+
+}
