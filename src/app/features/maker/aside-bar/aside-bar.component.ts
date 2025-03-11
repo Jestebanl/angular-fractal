fc757c977b26c25a3e2770ca7ccfc6b7cd6a0731
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-11 17:39:12 +0100

    primeros pasos en el maker

--- src/app/features/maker/aside-bar/aside-bar.component.ts
+++ src/app/features/maker/aside-bar/aside-bar.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aside-bar',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './aside-bar.component.html',
   styleUrl: './aside-bar.component.css'
 })
-export class AsideBarComponent {
+export class AsideBarComponent implements OnInit {
+  isDarkMode: boolean = false;
 
+  ngOnInit() {
+    this.loadDarkModePreference();
+  }
+  
+  loadDarkModePreference() {
+    const prefersDarkMode = localStorage.getItem('dark-mode') === 'true';
+    this.isDarkMode = prefersDarkMode;
+    if (prefersDarkMode) {
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  toggleDarkMode() {
+    const htmlElement = document.documentElement;
+    this.isDarkMode = htmlElement.classList.toggle('dark');
+    localStorage.setItem('dark-mode', this.isDarkMode.toString());
+  }
 }
--- src/app/features/maker/layout/layout.component.ts
+++ src/app/features/maker/layout/layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ArtboardComponent } from "../artboard/artboard.component";
+import { AsideBarComponent } from "../aside-bar/aside-bar.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [ArtboardComponent, AsideBarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export default class LayoutComponent {
 
 }
