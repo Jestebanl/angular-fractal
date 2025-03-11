1ed1cd706391918078bcf5293353ab1b37552be7
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-11 19:55:07 +0100

    lista de componentes preview

--- src/app/features/maker/aside-bar/aside-bar.component.ts
+++ src/app/features/maker/aside-bar/aside-bar.component.ts
@@ -1,15 +1,36 @@
-import { NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aside-bar',
-  imports: [NgIf],
+  imports: [NgIf,NgFor,NgClass],
   templateUrl: './aside-bar.component.html',
   styleUrl: './aside-bar.component.css'
 })
 export class AsideBarComponent implements OnInit {
   isDarkMode: boolean = false;
-
+  categories = [
+    {
+      name: 'Componentes de UI',
+      isOpen: false,
+      components: ['Botón', 'Formulario', 'Tabla', 'Modal']
+    },
+    {
+      name: 'Services',
+      isOpen: false,
+      components: ['AuthService', 'DataService', 'ApiService']
+    },
+    {
+      name: 'Guards',
+      isOpen: false,
+      components: ['AuthGuard', 'RoleGuard', 'CanDeactivateGuard']
+    },
+    {
+      name: 'Modelos',
+      isOpen: false,
+      components: ['Usuario', 'Producto', 'Pedido']
+    }
+  ];
   ngOnInit() {
     this.loadDarkModePreference();
   }
