7342ef649027ae332269b7311f98ee04747f411f
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-03-12 10:52:12 +0100

    estilos toast heredados del padre

--- src/app/features/maker/components/toast-libreria/toast-libreria.component.ts
+++ src/app/features/maker/components/toast-libreria/toast-libreria.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-toast-libreria',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toast-libreria.component.html',
   styles: ``
 })
 export class ToastLibreriaComponent {
-
+  @Input() backgroundColor: string = 'text-green-800 bg-green-50 dark:text-green-400 dark:bg-gray-800';
+  @Input() iconBackgroundColor: string = 'text-green-500 bg-green-100 dark:bg-green-800 dark:text-green-200';
+  @Input() iconColor: string = 'text-green-500';
+  @Input() iconPath: string = 'M5 13l4 4L19 7';
+  @Input() iconDescription: string = 'UI component icon';
+  @Input() textColor: string = 'text-gray-500 dark:text-gray-400';
+  @Input() message: string = 'Ejemplo de componente de ui';
 }
